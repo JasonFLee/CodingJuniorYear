bcacae093408f2acf91799f3b3c86e8e3ca469cc
JasonFLee <142084@student.asd20.org>
2018-11-08 14:31:51 -0700

    has uncompled metheod notes

---
+++ Archive 11-2-2018/AcceptsDoubleResetsTo2/AcceptsDoubleResetsTo2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcceptsDoubleResetsTo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type in double");
+
+            double w = double.Parse(Console.ReadLine());
+
+
+
+
+
+
+
+        }
+        static void Print(double w)
+        {
+         
+
+
+
+
+        }
+        
+
+    }
+}
---
+++ Archive 11-2-2018/MetheodOverloading/MetheodOverloading/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetheodOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        static void Print(int a)
+        {
+
+        }
+        static void Print(int[] array)
+        {
+
+        }
+        static void Print(int[,] array)
+        {
+
+        }
+        static void Print(int[][] array)
+        {
+
+        }
+    }
+}
