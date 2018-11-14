b0a41e66922603aa53787b9d814d8d25b0487ab9
JasonFLee <142084@student.asd20.org>
2018-11-14 14:22:58 -0700

    done

---
+++ UsesTextFiles/UsesTextFiles/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace UsesTextFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+ string path = @"C:\Users\142084\OneDrive - Academy District 20\C#\WillIWinOrLooseTheFight.txt";
+
+
+
+
+
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                int x=0;
+                Random rand = new Random();
+                int dice = rand.Next(0, 2);
+                if(dice == 0)
+                {
+                    do
+                    {
+                        x = x + 1;
+                        sw.WriteLine("W");
+                    } while (x < 100);
+                }
+                else if (dice == 1)
+                {
+                    do
+                    {
+                        x = x + 1;
+                        sw.WriteLine("L");
+                    } while (x < 100);
+                }
+
+            }
+            path = AppDomain.CurrentDomain.BaseDirectory + @"Example.txt";
+
+          
+        }
+        static void eternalize()
+        {
+            string path = @"C:\Users\142084\OneDrive - Academy District 20\C#\WillIWinOrLooseTheFight.txt";
+
+            List<string> file = new List<string>();
+            using (Stream sr = new StreamReader(path))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    file.Add(line);
+                }
+            }
+        }
+
+
+
+
+       
+
+    }
+}
