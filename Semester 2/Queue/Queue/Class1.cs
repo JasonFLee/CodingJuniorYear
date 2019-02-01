b4c5b9c38b023741bffd7821bf7ff64ce7dba835
JasonFLee <142084@student.asd20.org>
2019-02-01 13:33:13 -0700

    qeeueueueu

---
+++ Semester 2/Queue/Queue/Class1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue myqueue = new Queue();
+            int selection;
+            do
+            {
+                Console.WriteLine("1) EnQueue");
+                Console.WriteLine("2) Peek");
+                Console.WriteLine("3) DeQueue");
+                Console.WriteLine("4) Print");
+                Console.WriteLine("5) Search");
+                Console.WriteLine("6) Exit");
+
+
+
+                Console.WriteLine("What do you want to do?");
+                selection = int.Parse(Console.ReadLine());
+                if (selection == 1)
+                {
+                    Console.WriteLine("What number do you want to add to your queue?");
+                    int pushadd = int.Parse(Console.ReadLine());
+                    myqueue.EnQueue(pushadd);
+                    Console.WriteLine("Done");
+
+                }
+                if (selection == 2)
+                {
+                    Console.WriteLine("The top value is a " + myqueue.Peek());
+                }
+                if (selection == 3)
+                {
+
+                    
+
+                    int popval = myqueue.DeQueue();
+                    
+                    if(popval == -1)
+                    {
+                        Console.WriteLine("Can't, your queue is empty");
+                    }
+                    else { Console.WriteLine("The top value of the queue is a " + popval); }
+                        
+                    
+
+
+
+
+                }
+                if (selection == 4)
+                {
+
+                    myqueue.Print();
+
+                }
+                if (selection == 5)
+                {
+
+                    myqueue.share();
+                }
+
+            } while (selection != 6);
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Semester 2/Queue/Queue/Queue.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    class Queue
+    {
+        List<int> holder = new List<int>();
+        public void EnQueue(int val)
+        {
+            holder.Add(val);
+        }
+        //pop
+        public int DeQueue()
+        {
+            if (holder.Count != 0)
+            {
+                int top = holder[0];
+                holder.RemoveAt(0);
+                return top;
+            }
+            return -1;
+        }
+        //peek
+        public int Peek()
+        {
+            int top2 = holder[0];
+
+
+            return top2;
+        }
+        //print
+        public void Print()
+        {
+            Console.WriteLine("The Queue: ");
+            foreach (object o in holder)
+            {
+                Console.WriteLine(o);
+            }
+
+
+            
+            Console.WriteLine(" ");
+        }
+        public void share()
+        {
+
+            Console.WriteLine("Which number do you want to look up?");
+
+            int lookup = int.Parse(Console.ReadLine());
+            for(int v = 0; v < 10000; v ++)
+            {
+                if (lookup == holder[v])
+                {
+                    Console.WriteLine(lookup+ " is in spot "+ v);
+                    v = v + 10060;
+                }
+            }
+
+                
+            }
+
+
+
+
+        
+    }
+}
+
